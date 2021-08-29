e7380196e66678c3bf0b5c339b151338cf871ac6
devkhoa <khoale548@gmail.com>
2021-08-29 20:01:00 +0700

    push code

---
+++ Js/app.js
@@ -0,0 +1,40 @@
+$('.packages-btn').click(function(){
+    $('.header-menu-mobile .subMobile').toggleClass('show')
+    $('.header-menu__list-mobile .iconsMobileDown').toggleClass('rotate')
+})
+$('.tour-btn').click(function(){
+    $('.subMobile__items .subMobile__listChild').toggleClass('show')
+    $('.subMobile__items .iconsMobileDown-2').toggleClass('rotate')
+})
+$('.shop-btn').click(function(){
+    $('.header-menu__list-items-mobile .subMobile-1').toggleClass('show')
+    $('.header-menu__list-items-mobile .iconsMobileDown-1-2').toggleClass('rotate')
+})
+$('.aboutus-btn').click(function(){
+    $('.header-menu__list-items-mobile .subMobile-2').toggleClass('show')
+    $('.header-menu__list-items-mobile .iconsMobileDown-1-3').toggleClass('rotate')
+})
+$('.pages-btn').click(function(){
+    $('.header-menu__list-items-mobile .subMobile-3').toggleClass('show')
+    $('.header-menu__list-items-mobile .iconsMobileDown-1-4').toggleClass('rotate')
+})
+$('.aboutus2-btn').click(function(){
+    $('.subMobile__listChild-1').toggleClass('show')
+    $('.iconsMobileDown-2-1').toggleClass('rotate')
+})
+$('.contact-btn').click(function(){
+    $('.subMobile__listChild-2').toggleClass('show')
+    $('.iconsMobileDown-2-2').toggleClass('rotate')
+})
+$('.news-btn').click(function(){
+    $('.subMobile-4').toggleClass('show')
+    $('.iconsMobileDown-1-5').toggleClass('rotate')
+})
+$('.contact2-btn').click(function(){
+    $('.subMobile-5').toggleClass('show')
+    $('.iconsMobileDown-1-6').toggleClass('rotate')
+})
+$('.post-btn').click(function(){
+    $('.subMobile__listChild-3').toggleClass('show')
+    $('.iconsMobileDown-2-3').toggleClass('rotate')
+})
